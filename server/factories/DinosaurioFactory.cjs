783fd7c546431529361125b5ea0424c2f1e51709
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-05-15 02:01:27 +0200

    Update DinosaurioFactory.cjs

--- server/factories/DinosaurioFactory.cjs
+++ server/factories/DinosaurioFactory.cjs
@@ -2,27 +2,27 @@ const { fakerES } = require('@faker-js/faker')
 
 const dinosaurios = async () => {
     const dino = []
-    for (let i = 1; i < 17; i++) {
+    for (let i = 1; i <= 17; i++) {
         const raza = i
-        let alimentacion = 0
-        let nivelPeligrosidad = 0
+        let alimentacion = 1
+        let nivelPeligrosidad = 1
 
         if (raza >= 7 && raza <= 9) {
-            alimentacion = 1
-        }else if (raza >= 10 && raza <= 17) {
             alimentacion = 2
+        }else if (raza >= 10 && raza <= 17) {
+            alimentacion = 3
         }
 
         if (raza == 1 || raza == 3 || raza == 4 || raza == 7 || raza == 8) {
-            nivelPeligrosidad = 1
-        }else if (raza == 9) {
             nivelPeligrosidad = 2
-        }else if (raza >= 10 && raza <= 13) {
+        }else if (raza == 9) {
             nivelPeligrosidad = 3
-        }else if (raza >= 14 && raza <= 16) {
+        }else if (raza >= 10 && raza <= 13) {
             nivelPeligrosidad = 4
-        }else{
+        }else if (raza >= 14 && raza <= 16) {
             nivelPeligrosidad = 5
+        }else if (raza == 17){
+            nivelPeligrosidad = 6
         }
 
         dino.push({
